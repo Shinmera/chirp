4fdfc1a1d6300327c484778711406db9fdbbf22b
Shinmera <shinmera@tymoon.eu>
2014-02-18 22:30:31 +0100

    Completed OAuth functionality. SERVER method has yet to be tested.

--- oauth.lisp
+++ oauth.lisp
@@ -19,6 +19,7 @@
 (defvar *oauth/authenticate* "https://api.twitter.com/oauth/authenticate")
 (defvar *oauth/authorize* "https://api.twitter.com/oauth/authorize")
 (defvar *oauth/access-token* "https://api.twitter.com/oauth/access_token")
+(defvar *server-port* 8989)
 
 (define-condition oauth-error (error) ())
 
@@ -48,12 +49,15 @@
 (defun parse-boolean (value)
   (when (string= value "true") T))
 
+(defun to-keyword (string)
+  (intern (cl-ppcre:regex-replace-all "_" (string-upcase string) "-") "KEYWORD"))
+
 (defun oauth-response->alist (body &optional spec)
   "Turn an oauth-response into an ALIST."
   (mapcar #'(lambda (assignment)
               (let* ((pair (split-sequence #\= assignment))
-                     (key (intern (cl-ppcre:regex-replace-all "_" (string-upcase (car pair)) "-")))
-                     (val (cdr pair))
+                     (key (to-keyword (first pair)))
+                     (val (second pair))
                      (parser (cdr (assoc key spec))))
                 (cons key (if parser (funcall parser val) val))))
           (split-sequence #\& body)))
@@ -135,7 +139,8 @@ According to spec https://dev.twitter.com/docs/auth/authorizing-request"
                               ("oauth_nonce" . ,(generate-nonce))
                               ("oauth_signature_method" . ,*oauth-signature-method*)
                               ("oauth_timestamp" . ,(write-to-string (get-unix-time)))
-                              ("oauth_version" . ,*oauth-version*))))
+                              ("oauth_version" . ,*oauth-version*))
+                            (when *oauth-token* `(("oauth_token" . ,*oauth-token*)))))
          (oauth-parameters (make-signed method request-url oauth-parameters))
          (headers `(("Authorization" . ,(create-authorization-header oauth-parameters)))))
     (let ((result (multiple-value-list (drakma:http-request
@@ -146,7 +151,7 @@ According to spec https://dev.twitter.com/docs/auth/authorizing-request"
                  :body (first result) :status (second result) :headers (third result)
                  :url request-url :method method :parameters parameters :sent-headers headers)))))
 
-(defun callback-request-token (callback)
+(defun request-token (callback)
   "Query for a request token using the specified callback.
 Returns an ALIST containing :OAUTH-TOKEN, :OAUTH-TOKEN-SECRET and 
 :OAUTH-CALLBACK-CONFIRMED, the first two being strings and the last a boolean.
@@ -154,11 +159,90 @@ Returns an ALIST containing :OAUTH-TOKEN, :OAUTH-TOKEN-SECRET and
 According to spec https://dev.twitter.com/docs/auth/implementing-sign-twitter"
   (oauth-response->alist
    (signed-request *oauth/request-token* :oauth-parameters `(("oauth_callback" . ,callback)))
-   `((:OAUTH-CALLBACK-CONFIRMED . #'parse-boolean))))
+   `((:OAUTH-CALLBACK-CONFIRMED . ,#'parse-boolean))))
 
 (defun pin-request-token ()
   "Query for a PIN based request token.
 Seee CALLBACK-REQUEST-TOKEN.
 
 According to spec https://dev.twitter.com/docs/auth/pin-based-authorization"
-  (callback-request-token "oob"))
+  (request-token "oob"))
+
+(defun initiate-redirect-authentication (callback-url)
+  "Initiate the authentication through the redirect mechanism.
+Returns an URL that the user has to open in the browser.
+Upon successful authentication, the page should redirect back
+to the specified callback url. This callback endpoint should then
+pass the proper parameters to COMPLETE-AUTHENTICATION.
+
+According to spec https://dev.twitter.com/docs/auth/implementing-sign-twitter"
+  (let ((data (request-token callback-url)))
+    (setf *oauth-token* (cdr (assoc :oauth-token data))
+          *oauth-token-secret* (cdr (assoc :oauth-token-secret data)))
+    (format NIL "~a?oauth_token=~a" *oauth/authorize* *oauth-token*)))
+
+(defun initiate-pin-authentication ()
+  "Initiate the authentication through the PIN mechanism.
+Returns an URL that the user has to open in the browser.
+This page should, upon successful authentication, return a PIN
+that has to be initialized by passing it to COMPLETE-AUTHENTICATION.
+
+According to spec https://dev.twitter.com/docs/auth/pin-based-authorization"
+  (initiate-redirect-authentication "oob"))
+
+(defun initiate-server-authentication ()
+  "Initiate the authentication through the server mechanism.
+This is the same as the redirect mechanism, except it starts up
+a Hunchentoot server automatically and handles the callback for
+you. The server will be started on *SERVER-PORT* and will be shut
+down automatically after a single request."
+  (assert (asdf:find-system "hunchentoot") () "Please install hunchentoot before using this authentication method.")
+  (asdf:load-system "hunchentoot")
+  (labels ((ht-symb (name)
+             (find-symbol name "HUNCHENTOOT"))
+           (ht-func (name &rest params)
+             (apply (symbol-function (ht-symb name)) params)))
+    (let ((server (ht-func "START" (make-instance (ht-symb "EASY-ACCEPTOR") :port *server-port*)))
+          (standard-output *standard-output*))
+      (flet ((dispatcher (request)
+               (unless (cl-ppcre:scan  "favicon\.ico$" (ht-func "SCRIPT-NAME" request))
+                 (lambda (&rest args)
+                   (declare (ignore args))
+                   (unwind-protect
+                        (progn
+                          (complete-authentication
+                           (ht-func "GET-PARAMETER" "oauth_verifier")
+                           (ht-func "GET-PARAMETER" "oauth_token"))
+                          (format standard-output "CHIRP-OAUTH: Authentication completed."))
+                     (format standard-output "CHIRP-OAUTH: Shutting down server.")
+                     (ht-func "STOP" server))))))
+        (push #'dispatcher (symbol-value (ht-symb "*DISPATCH-TABLE*")))
+        (initiate-redirect-authentication (format NIL "http://localhost:~d/callback" *server-port*))))))
+
+(defun initiate-authentication (&key (method :PIN) (consumer-key *oauth-consumer-key*) (consumer-secret *oauth-consumer-secret*))
+  "Starts the authentication process and returns an URL that the user has to visit.
+METHOD can be one of :PIN :SERVER or a string designating a callback URL.
+See INITIATE-PIN-AUTHENTICATION, INITIATE-SERVER-AUTHENTICATION and
+INITIATE-REDIRECT-AUTHENTICATION respectively."
+  (setf *oauth-consumer-key* consumer-key
+        *oauth-consumer-secret* consumer-secret)
+  (case method
+    (:PIN (initiate-pin-authentication))
+    (:SERVER (initiate-server-authentication))
+    (T (initiate-redirect-authentication method))))
+
+(defun access-token (verifier)
+  "Turn the tokens received through the authentication into an access token.
+
+According to spec https://dev.twitter.com/docs/auth/implementing-sign-twitter"
+  (oauth-response->alist
+   (signed-request *oauth/access-token* :parameters `(("oauth_verifier" . ,verifier)))))
+
+(defun complete-authentication (verifier &optional (token *oauth-token*))
+  "Finishes the authentication procedure by retrieving the access token.
+Sets the *OAUTH-TOKEN* and *OAUTH-TOKEN-SECRET* to their respective values."
+  (setf *oauth-token* token)
+  (let ((data (access-token verifier)))
+    (setf *oauth-token* (cdr (assoc :oauth-token data))
+          *oauth-token-secret* (cdr (assoc :oauth-token-secret data)))
+    (values *oauth-token* *oauth-token-secret*)))
