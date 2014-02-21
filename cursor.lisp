adb818f91b3332215fdf74c59a9700eb30a6f8d4
Shinmera <shinmera@tymoon.eu>
2014-02-21 08:38:17 +0100

    Added request-method parameter

--- cursor.lisp
+++ cursor.lisp
@@ -7,23 +7,23 @@
 (in-package #:org.tymoonnext.chirp)
 
 (defclass* cursor ()
-  (id url parameters data)
+  (id url parameters data request-method)
   (:documentation "Cursor object to traverse cursored sets.
 
 According to https://dev.twitter.com/docs/misc/cursoring"))
 
-(defun cursored-request (uri &key parameters)
+(defun cursored-request (uri &key parameters (method :GET))
   "Performs a signed-request returning a cursor instance tailored for it."
   (cursor-request
    (make-instance
-    'cursor :id -1 :url uri :parameters parameters)))
+    'cursor :id -1 :url uri :parameters parameters :request-method method)))
 
 (defgeneric cursor-request (cursor)
   (:documentation "Perform the currently stored cursor request and save the data in the cursor object."))
 
 (defmethod cursor-request ((cursor cursor))
   (let ((data (signed-request (url cursor) :parameters (append (parameters cursor)
-                                                               `(("cursor" . ,(id cursor)))) :method :GET)))
+                                                               `(("cursor" . ,(id cursor)))) :method (request-method cursor))))
     (setf (data cursor) data)
     (values cursor data)))
 
@@ -51,26 +51,26 @@ If no additional data is available, NIL is returned instead."))
       (setf (id cursor) previous-id)
       (cursor-request cursor))))
 
-(defmacro do-cursor ((data-var uri &key parameters (cursor-var (gensym "CURSOR"))) &body body)
+(defmacro do-cursor ((data-var uri &key parameters (method :GET) (cursor-var (gensym "CURSOR"))) &body body)
   "Iterates over all data sets of a cursor, binding the request data to DATA-VAR on each iteration.
 Returns the cursor at its end position. "
-  `(loop with ,cursor-var = (cursored-request ,uri :parameters ,parameters)
+  `(loop with ,cursor-var = (cursored-request ,uri :parameters ,parameters :method ,method)
          for ,data-var = (data ,cursor-var)
          do (progn ,@body)
          while (cursor-next ,cursor-var)
          finally (return ,cursor-var)))
 
-(defun map-cursor (function parameter uri &key parameters)
+(defun map-cursor (function parameter uri &key parameters (method :GET))
   "Applies FUNCTION to each element of the designated parameter while going through the cursor.
 Returns the collected return values of the FUNCTION calls."
   (let ((result ()))
-    (do-cursor (data uri :parameters parameters)
-      (appendf result (mapcar function (cdr (assoc parameter data)))))
+    (do-cursor (data uri :parameters parameters :method method)
+      (appendf result (mapcar function (if parameter (cdr (assoc parameter data)) data))))
     result))
 
-(defun cursor-collect (parameter uri &key parameters)
+(defun cursor-collect (parameter uri &key parameters (method :GET))
   "Collects the given parameter into one list by APPEND."
   (let ((result ()))
-    (do-cursor (data uri :parameters parameters)
-      (appendf result (cdr (assoc parameter data))))
+    (do-cursor (data uri :parameters parameters :method method)
+      (appendf result (if parameter (cdr (assoc parameter data)) data)))
     result))
