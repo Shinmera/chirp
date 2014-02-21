2d3eebc81b371bffd1d853e129db9ce20355abf1
Shinmera <shinmera@tymoon.eu>
2014-02-21 22:09:45 +0100

    Fixed search

--- search.lisp
+++ search.lisp
@@ -18,7 +18,7 @@
   meta)
 
 (define-make-* (search-metadata)
-  :max-id :since-id :refresh-url :next-results (:count . :result-count) :completed-in :query)
+  :max-id :since-id :refresh-url :next-results (:result-count . :count) :completed-in :query)
 
 (defun search/tweets (query &key latitude longitude radius locale language (result-type :mixed) (count 15) until since-id max-id (include-entities T))
   "Returns a collection of relevant Tweets matching a specified query and some search metadata as a second value.
@@ -30,13 +30,13 @@ According to spec https://dev.twitter.com/docs/api/1.1/get/search/tweets"
   (assert (member result-type '(:mixed :recent :popular)) () "MEMBER must be one of :MIXED :RECENT :POPULAR")
   (assert (<= count 100) () "COUNT cannot be larger than 100.")
   (etypecase until
-    (string)
+    (string) (null)
     (local-time:timestamp
      (setf until (local-time:format-timestring NIL until :format '((:year 4) #\- (:month 2) #\- (:day 2))))))
   (unless include-entities (setf include-entities "false"))
-  (let* ((geocode (format NIL "~a,~a,~a" latitude longitude radius))
+  (let* ((geocode (when latitude (format NIL "~a,~a,~a" latitude longitude radius)))
          (data (signed-request *search/tweets*
-                               :parameters (prepare* query geocode locale language result-type count until since-id max-id include-entities)
+                               :parameters (prepare* (q . query) geocode locale language result-type count until since-id max-id include-entities)
                                :method :GET)))
     (values (mapcar #'make-status (cdr (assoc :statuses data)))
             (make-search-metadata (cdr (assoc :search-metadata data))))))
