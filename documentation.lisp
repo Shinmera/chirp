e09a302ab24ab3bfd83b04375f591e0746da9bfe
Shinmera <shinmera@tymoon.eu>
2014-02-21 09:41:30 +0100

    Added documentation generation file

---
+++ documentation.lisp
@@ -0,0 +1,23 @@
+#|
+ This file is a part of Chirp
+ (c) 2013 TymoonNET/NexT http://tymoon.eu (shinmera@tymoon.eu)
+ Author: Nicolas Hafner <shinmera@tymoon.eu>
+|#
+
+(defpackage #:org.tymoonnext.chirp.doc
+  (:use #:cl #:lquery #:lquery-doc)
+  (:nicknames #:chirp-doc)
+  (:export #:build-documentation))
+
+(in-package #:org.tymoonnext.chirp.doc)
+
+(defmethod documentate-object :after (template object fields)
+  ($ template ".anchor" (attr :name (symbol-name (nth 0 object)))))
+
+(defun build-documentation ()
+  ($ (initialize (merge-pathnames "about-template.html" (asdf:system-source-directory :chirp))))
+  (let ((template ($ "#template")))
+    (let ((nodes (lquery-doc::documentate template :chirp :exclude '(:internal :method))))
+      ($ "#docs" (empty) (append nodes))))
+  ($ (write-to-file (merge-pathnames "about.html" (asdf:system-source-directory :chirp)))))
+
