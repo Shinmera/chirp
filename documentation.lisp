98ab548bcd57b12516adcf95537268134f3f4b8f
Shinmera <shinmera@tymoon.eu>
2014-03-06 16:01:47 +0100

    Made documentation fit new package format and added links

--- documentation.lisp
+++ documentation.lisp
@@ -12,12 +12,17 @@
 (in-package #:org.tymoonnext.chirp.doc)
 
 (defmethod documentate-object :after (template object fields)
-  ($ template ".anchor" (attr :name (symbol-name (nth 0 object)))))
+  ($ template ".anchor" (attr :name (symbol-name (nth 0 object))))
+  ($ template "h3 a" (attr :href (format NIL "#~a" (symbol-name (nth 0 object))))))
 
 (defun build-documentation ()
   ($ (initialize (merge-pathnames "about-template.html" (asdf:system-source-directory :chirp))))
   (let ((template ($ "#template")))
-    (let ((nodes (lquery-doc::documentate template :chirp :exclude '(:internal :method))))
-      ($ "#docs" (empty) (append nodes))))
+    (let ((nodes (lquery-doc::documentate template :chirp-api :exclude '(:internal :method))))
+      ($ "#docs-api" (empty) (append nodes)))
+    (let ((nodes (lquery-doc::documentate template :chirp-extra :exclude '(:internal :method))))
+      ($ "#docs-extra" (empty) (append nodes)))
+    (let ((nodes (lquery-doc::documentate template :chirp-objects :exclude '(:internal :method))))
+      ($ "#docs-objects" (empty) (append nodes))))
   ($ (write-to-file (merge-pathnames "about.html" (asdf:system-source-directory :chirp)))))
 
